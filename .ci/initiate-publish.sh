9ea6d1be3213770d65409901be01e9127b9216e4
Ned Twigg <ned.twigg@diffplug.com>
2015-05-27 11:08:24 -0700

    Added the ci scripts.

---
+++ .ci/initiate-publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This script initiates the Gradle publishing task when pushes to master occur.
+# NOTE: Travis-CI can only publish SNAPSHOT versions.
+if [ "$TRAVIS_REPO_SLUG" == "diffplug/durian-rx" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+	if [[ $(./gradlew -q printVersion) != *SNAPSHOT* ]]; then
+		echo -e "Publishing snapshot\n"
+	else
+		echo -e "Publishing release\n"
+	fi
+
+	./gradlew publish
+	RETVAL=$?
+
+	if [ $RETVAL -eq 0 ]; then
+		echo 'Completed publish!'
+	else
+		echo 'Publish failed.'
+		return 1
+	fi
+fi
---
+++ .ci/push-javadoc-to-gh-pages.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# This script initiates publishing the proper Javadoc page whenever pushes to master occur.
+# NOTE: Travis-CI can only publish SNAPSHOT versions.
+if [ "$TRAVIS_REPO_SLUG" == "diffplug/durian-rx" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+	echo -e "Publishing javadoc...\n"
+
+	# copy javadoc from the build to ~/javadoc-temp
+	cp -R build/docs/javadoc $HOME/javadoc-temp
+
+	# clone a new copy durian-rx to gh-pages
+	cd $HOME
+	git config --global user.email "travis@travis-ci.org"
+	git config --global user.name "travis-ci"
+	rm -rf $HOME/gh-pages
+	git clone --quiet --branch=gh-pages https://${gh_token}@github.com/diffplug/durian-rx gh-pages > /dev/null
+
+	# copy the javadoc into the build
+	cd gh-pages
+	VERSION=$(./gradlew -q printVersion)
+	if [[ "$VERSION" != *SNAPSHOT* ]]; then
+		git rm -rf javadoc/${VERSION}
+		cp -Rf $HOME/javadoc-temp/ ./javadoc/${VERSION}/
+	else
+		git rm -rf javadoc/latest
+		cp -Rf $HOME/javadoc-temp/ ./javadoc/latest/
+	fi
+
+	# add all of the stuff and commit it
+	git add -f -A
+	git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+	git push -fq origin gh-pages > /dev/null
+
+	# success
+	echo -e "Published Javadoc to gh-pages.\n"
+fi
