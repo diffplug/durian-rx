d0c3be5813ffe7c5b1ee50ea9a0186383676f2ad
Ned Twigg <ned.twigg@diffplug.com>
2015-06-17 21:46:16 -0700

    Added the enforce() method to RxBox.

--- src/com/diffplug/common/rx/RxBox.java
+++ src/com/diffplug/common/rx/RxBox.java
@@ -25,6 +25,7 @@ import rx.subjects.BehaviorSubject;
 import com.google.common.base.Preconditions;
 
 import com.diffplug.common.base.Box;
+import com.diffplug.common.base.Consumers;
 
 /**
  * {@link RxGetter} and {@link Box} combined in one: a value you can set, get, and subscribe to.
@@ -38,6 +39,52 @@ public interface RxBox<T> extends RxGetter<T>, Box<T> {
 	/** Maps one {@code RxBox} to another {@code RxBox}. */
 	default <R> RxBox<R> map(Function<? super T, ? extends R> getMapper, Function<? super R, ? extends T> setMapper) {
 		return from(map(getMapper), toSet -> set(setMapper.apply(toSet)));
+	}
+
+	/**
+	 * Provides a mechanism for enforcing an invariant.
+	 * <p>
+	 * For example, let's say that there is an {@code RxBox<List<T>>} which represents
+	 * a user's multiselection in some widget. You would like the user to select
+	 * a pair of values - you want to enforce that the selection doesn't grow
+	 * beyond 2 elements.
+	 * <p>
+	 * You could write code like this:
+	 * <pre>
+	 * {@code
+	 * RxBox<List<T>> selection = widget.rxSelection();
+	 * RxGetter<List<T>> onlyPairs = selection.map(list -> list.stream().limit(2).collect(Collectors.toList()));
+	 * Rx.subscribe(onlyPairs, selection);
+	 * }
+	 * </pre>
+	 * But there's a subtle bug in this code.  Because {@link RxGetter} and {@link RxBox} only
+	 * fire their observables when their value changes, the selection invariant won't be enforced
+	 * unless {@code onlyPairs} actually changes.  It's easy to fix:
+	 * <pre>
+	 * {@code
+	 * RxBox<List<T>> selection = widget.rxSelection();
+	 * Observable<List<T>> onlyPairs = selection.asObservable().map(list -> list.stream().limit(2).collect(Collectors.toList()));
+	 * Rx.subscribe(onlyPairs, selection);
+	 * }
+	 * </pre>
+	 * But there's still a problem - {@code selection} will momentarily contain values which violate the invariant. That
+	 * means you can't write listener code that relies on the invariant being true.  Which is why the {@code enforce()} method exists:
+	 * <pre>
+	 * {@code
+	 * RxBox<List<T>> onlyPairs = widget.rxSelection().enforce(list -> list.stream().limit(2).collect(Collectors.toList()));
+	 * }
+	 * </pre>
+	 * This creates a new {@code RxBox} which will always satisfy the invariant, and it will enforce this invariant
+	 * on the original {@code RxBox}. 
+	 */
+	default RxBox<T> enforce(Function<? super T, ? extends T> getMapper) {
+		// this must be a plain-old observable, because it needs to fire
+		// every time an invariant is violated, not only when a violation
+		// of the invariant causes a change in the output
+		Observable<T> mapped = asObservable().map(getMapper::apply);
+		Rx.subscribe(mapped, this::set);
+		// now we can return the RxBox
+		return from(map(getMapper), Consumers.compose(getMapper, this::set));
 	}
 
 	/** Creates an {@code RxBox} with the given initial value. */
