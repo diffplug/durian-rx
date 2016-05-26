f0bcba2538c324a342bd0a9a4d30a17126e075b1
Ned Twigg <ned.twigg@diffplug.com>
2016-05-26 03:17:22 -0700

    Added LockBox and RxLockBox, which use locks to ensure atomicity, and OrderedLocks to prevent deadlock under atomicity.

---
+++ src/com/diffplug/common/rx/LockBox.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2016 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.rx;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import com.diffplug.common.base.Box;
+import com.diffplug.common.base.ConverterNonNull;
+
+/**
+ * LockBox is a box where every call to {@link #modify(Function)}
+ * happens within a synchronized block.
+ * 
+ * Using the {@link #transactOn(LockBox...)} method, you can obtain a lock
+ * on multiple boxes in a way which is guaranteed to be free
+ * of deadlock, so long as no one is getting a lock except through
+ * {@link #modify(Function)} and {@link #transactOn(LockBox...)}.
+ */
+public interface LockBox<T> extends Box<T> {
+	/**
+	 * The lock which is used by this LockBox's {@link #modify(Function) method}.
+	 * 
+	 * For a LockBox which holds state, the LockBox itself is used.  For a mapped
+	 * LockBox, the underlying LockBox which actually holds the state is used.
+	 */
+	Object lock();
+
+	@Override
+	default T modify(Function<? super T, ? extends T> mutator) {
+		synchronized (lock()) {
+			set(mutator.apply(get()));
+			return get();
+		}
+	}
+
+	/** Creates a `LockBox` containing the given value. */
+	public static <T> LockBox<T> of(T value) {
+		return new Default<>(value);
+	}
+
+	static class Default<T> implements LockBox<T> {
+		protected T value;
+
+		protected Default(T value) {
+			set(value);
+		}
+
+		@Override
+		public Object lock() {
+			return this;
+		}
+
+		@Override
+		public T get() {
+			return value;
+		}
+
+		@Override
+		public void set(T value) {
+			this.value = Objects.requireNonNull(value);
+		}
+
+		@Override
+		public String toString() {
+			return "LockBox.of[" + get() + "]";
+		}
+	}
+
+	/**
+	 * Maps this LockBox to a new value which will have the
+	 * same lock as the original lock, since there's still
+	 * only one piece of state.
+	 */
+	@Override
+	default <R> Box<R> map(ConverterNonNull<T, R> converter) {
+		return new Mapped<>(this, converter);
+	}
+
+	static class Mapped<T, R> implements LockBox<R> {
+		private final LockBox<T> delegate;
+		private final ConverterNonNull<T, R> converter;
+
+		public Mapped(LockBox<T> delegate,
+				ConverterNonNull<T, R> converter) {
+			this.delegate = delegate;
+			this.converter = converter;
+		}
+
+		/**
+		 * Ensures that we use the root delegate which
+		 * is actually holding the state as our lock.
+		 */
+		@Override
+		public Object lock() {
+			return delegate.lock();
+		}
+
+		@Override
+		public R get() {
+			return converter.convert(delegate.get());
+		}
+
+		@Override
+		public void set(R value) {
+			delegate.set(converter.revert(value));
+		}
+
+		/** Shortcut for doing a set() on the result of a get(). */
+		@Override
+		public R modify(Function<? super R, ? extends R> mutator) {
+			Box.Nullable<R> result = Box.Nullable.ofNull();
+			delegate.modify(input -> {
+				R unmappedResult = mutator.apply(converter.convert(input));
+				result.set(unmappedResult);
+				return converter.revert(unmappedResult);
+			});
+			return result.get();
+		}
+
+		@Override
+		public String toString() {
+			return "[" + delegate + " mapped to " + get() + " by " + converter + "]";
+		}
+	}
+
+	/**
+	 * Creates an OrderedLock which allows running transactions on the given list of LockBoxes.
+	 * 
+	 * This OrderedLock can be reused, and it is efficient to do so.
+	 */
+	public static OrderedLock transactOn(@SuppressWarnings("rawtypes") LockBox... locks) {
+		return OrderedLock.on(Arrays.asList(locks).stream().map(LockBox::lock).collect(Collectors.toList()));
+	}
+}
---
+++ src/com/diffplug/common/rx/RxLockBox.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2016 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.rx;
+
+import rx.Observable;
+import rx.subjects.BehaviorSubject;
+
+import com.diffplug.common.base.ConverterNonNull;
+
+public interface RxLockBox<T> extends LockBox<T>, RxBox<T> {
+	/** RxLockBox must map to another kind of LockBox. */
+	@Override
+	<R> RxLockBox<R> map(ConverterNonNull<T, R> converter);
+
+	public static <T> RxLockBox<T> of(T value) {
+		return new Default<>(value);
+	}
+
+	static class Default<T> extends LockBox.Default<T> implements RxLockBox<T> {
+		BehaviorSubject<T> subject = BehaviorSubject.create();
+
+		protected Default(T value) {
+			super(value);
+			subject = BehaviorSubject.create();
+		}
+
+		@Override
+		public Observable<T> asObservable() {
+			return subject;
+		}
+
+		@Override
+		public <R> RxLockBox<R> map(ConverterNonNull<T, R> converter) {
+			return new RxLockBox.Mapped<>(this, converter);
+		}
+	}
+
+	static class Mapped<T, R> extends LockBox.Mapped<T, R> implements RxLockBox<R> {
+		final Observable<R> observable;
+
+		public Mapped(RxLockBox<T> delegate,
+				ConverterNonNull<T, R> converter) {
+			super(delegate, converter);
+			Observable<R> mapped = delegate.asObservable().map(converter::convert);
+			observable = mapped.distinctUntilChanged();
+		}
+
+		@Override
+		public Observable<R> asObservable() {
+			return observable;
+		}
+
+		@Override
+		public <V> RxLockBox<V> map(ConverterNonNull<R, V> converter) {
+			return new RxLockBox.Mapped<>(this, converter);
+		}
+	}
+}
