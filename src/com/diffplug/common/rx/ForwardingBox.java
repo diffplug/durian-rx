dc37423da88f050fd35e2e261e13227942064c57
Ned Twigg <ned.twigg@diffplug.com>
2016-05-28 00:16:26 -0700

    Candidate "ForwardingBox" implementation...

---
+++ src/com/diffplug/common/rx/ForwardingBox.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2016 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.common.rx;
+
+import java.util.function.Function;
+
+import rx.Observable;
+
+import com.diffplug.common.base.Box;
+
+public class ForwardingBox<T, BoxType extends Box<T>> implements Box<T> {
+	protected final BoxType delegate;
+
+	protected ForwardingBox(BoxType delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	public T get() {
+		return delegate.get();
+	}
+
+	@Override
+	public void set(T value) {
+		delegate.set(value);
+	}
+
+	@Override
+	public T modify(Function<? super T, ? extends T> mutator) {
+		return delegate.modify(mutator);
+	}
+
+	public static class Rx<T> extends ForwardingBox<T, RxBox<T>> implements RxBox<T> {
+		protected Rx(RxBox<T> delegate) {
+			super(delegate);
+		}
+
+		@Override
+		public Observable<T> asObservable() {
+			return delegate.asObservable();
+		}
+	}
+
+	public static class Lock<T> extends ForwardingBox<T, LockBox<T>> implements LockBox<T> {
+		protected Lock(LockBox<T> delegate) {
+			super(delegate);
+		}
+
+		@Override
+		public Object lock() {
+			return delegate.lock();
+		}
+	}
+
+	public static class LockRx<T> extends ForwardingBox<T, RxLockBox<T>> implements RxLockBox<T> {
+		protected LockRx(RxLockBox<T> delegate) {
+			super(delegate);
+		}
+
+		@Override
+		public Object lock() {
+			return delegate.lock();
+		}
+
+		@Override
+		public Observable<T> asObservable() {
+			return delegate.asObservable();
+		}
+	}
+}
