882a2e03715a24d9bbaa85d1349df73ecbcefaa3
Ned Twigg <ned.twigg@diffplug.com>
2016-06-04 11:23:06 -0700

    More build tweaks.

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -6,6 +6,6 @@
 if [ "$TRAVIS_REPO_SLUG" == "diffplug/durian-rx" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 	# Publish the artifacts
 	./gradlew publish || exit 1
-	# Publish the javadoc
-	./gradlew publishGhPages || exit 1
+	# Push the javadoc
+	./.ci/push-javadoc.sh
 fi
