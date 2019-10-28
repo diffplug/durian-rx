e6265a6f91854c01384db17a9ddd31ed813d6b9a
Ned Twigg <ned.twigg@diffplug.com>
2019-10-28 10:25:32 -0700

    Change javadoc links to javadoc.io

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -6,6 +6,4 @@
 if [ "$TRAVIS_REPO_SLUG" == "diffplug/durian-rx" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 	# Publish the artifacts
 	./gradlew publish || exit 1
-	# Push the javadoc
-	./gradlew publishGhPages || exit 1
 fi
