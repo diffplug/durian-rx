6f6d737f7790c74db4d9995a3cfeb0950851ebf2
Ned Twigg <ned.twigg@diffplug.com>
2020-01-13 11:28:45 -0800

    Stop publishing -SNAPSHOT

--- .ci/ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Do the Gradle build
-./gradlew build || exit 1
-
-if [ "$TRAVIS_REPO_SLUG" == "diffplug/durian-rx" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-	# Publish the artifacts
-	./gradlew publish || exit 1
-fi
